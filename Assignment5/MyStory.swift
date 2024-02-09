e66764ec74f5eaaf36729e4540423ad12f01291b
Jenna Marquardt <jsr0010@auburn.edu>
2024-02-09 17:17:37 -0600

    Added buttons and created main screen.

---
+++ Assignment5/MyStory.swift
@@ -0,0 +1,8 @@
+//
+//  MyStory.swift
+//  Assignment5
+//
+//  Created by Jenna Marquardt on 2/9/24.
+//
+
+import Foundation
---
+++ Assignment5/StoryModels.swift
@@ -0,0 +1,8 @@
+//
+//  StoryModels.swift
+//  Assignment5
+//
+//  Created by Jenna Marquardt on 2/9/24.
+//
+
+import Foundation
